1e872859cd7cda688ad3e21b1a5934a26cb9287f
Saygone <fordestiny1902@gmail.com>
2021-03-28 21:52:53 +0300

    Menu normal

---
+++ Menu.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+def clicked():
+    lbl.configure(text="Hачало игры", font=("Ariak Bold", 50))
+
+window = Tk()
+window.title("Space emulation")
+window.geometry('900x500')
+
+lbl = Label(window, text="Space emulation", font=("Ariak Bold", 50))
+# Font шрифт, число размер.
+lbl.place(x = 450, y = 150, anchor=CENTER)
+# Местоположение
+btn1 = Button(window, text="Запустить программу", fg="black", font=("Ariak Bold", 15), width=20,height=1, command=clicked)
+btn2 = Button(window, text="Выход", fg="Black", font=("Ariak Bold", 15), width=8,height=1, command=window.destroy)
+
+# Кнопка
+btn1.place(x = 450, y = 250, anchor=CENTER)
+btn2.place(x = 450, y = 295, anchor=CENTER)
+
+
+window.mainloop()
