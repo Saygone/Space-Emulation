b85ebffb508f78c8b1baa3e56681bb02d01eba6c
Saygone <fordestiny1902@gmail.com>
2021-04-05 14:18:43 +0300

    information

--- Menu.py
+++ Menu.py
@@ -14,11 +14,12 @@ lbl = Label(window, text="Space emulation", font=("Ariak Bold", 50))
 lbl.place(x = 450, y = 150, anchor=CENTER)
 # Местоположение
 btn1 = Button(window, text="Запустить программу", fg="black", font=("Ariak Bold", 15), width=20,height=1, command=clicked)
-btn2 = Button(window, text="Выход", fg="Black", font=("Ariak Bold", 15), width=8,height=1, command=window.destroy)
-
+btn2 = Button(window, text="Информация", fg="Black", font=("Ariak Bold", 15), width=20,height=1, command=clicked)
+btn3 = Button(window, text="Выход", fg="Black", font=("Ariak Bold", 15), width=20,height=1, command=window.destroy)
 # Кнопка
 btn1.place(x = 450, y = 250, anchor=CENTER)
 btn2.place(x = 450, y = 295, anchor=CENTER)
+btn3.place(x = 450, y = 340, anchor=CENTER)
 
 
 window.mainloop()
