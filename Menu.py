6a4807f03b0a9a735e8ab2d1156fe7381a7d7425
Saygone <fordestiny1902@gmail.com>
2021-04-05 15:04:11 +0300

    Almost ready

--- Menu.py
+++ Menu.py
@@ -1,7 +1,13 @@
 from tkinter import *
 
 def clicked():
-    lbl.configure(text="Hачало игры", font=("Ariak Bold", 50))
+    exec(open('game.py').read())
+
+
+def clicked_info():
+    exec(open('info.py').read())
+
+
 
 window = Tk()
 window.title("Space emulation")
@@ -13,7 +19,7 @@ lbl = Label(window, text="Space emulation", font=("Ariak Bold", 50))
 lbl.place(x = 450, y = 150, anchor=CENTER)
 # Местоположение
 btn1 = Button(window, text="Запустить программу", fg="black", font=("Ariak Bold", 15), width=20,height=1, command=clicked)
-btn2 = Button(window, text="Информация", fg="Black", font=("Ariak Bold", 15), width=20,height=1, command=clicked)
+btn2 = Button(window, text="Информация", fg="Black", font=("Ariak Bold", 15), width=20,height=1, command=clicked_info)
 btn3 = Button(window, text="Выход", fg="Black", font=("Ariak Bold", 15), width=20,height=1, command=window.destroy)
 # Кнопка
 btn1.place(x = 450, y = 250, anchor=CENTER)
