1e13188610c143a45de7eea47b381ba50e604b08
Saygone <fordestiny1902@gmail.com>
2021-05-24 11:05:49 +0300

    Осталась инфа

--- atlas.py
+++ atlas.py
@@ -2,12 +2,34 @@ import tkinter
 from tkinter import *
 from PIL import Image, ImageTk
 
+def clicked_sun():
+    exec(open('info.py').read())
 
-def clicked():
-    import game
+def clicked_mercury():
+    exec(open('info.py').read())
 
+def clicked_venus():
+    exec(open('info.py').read())
 
-def clicked_info():
+def clicked_earth():
+    exec(open('info.py').read())
+
+def clicked_mars():
+    exec(open('info.py').read())
+
+def clicked_jupiter():
+    exec(open('info.py').read())
+
+def clicked_saturn():
+    exec(open('info.py').read())
+
+def clicked_uranus():
+    exec(open('info.py').read())
+
+def clicked_neptune():
+    exec(open('info.py').read())
+
+def clicked_pluto():
     exec(open('info.py').read())
 
 
@@ -22,12 +44,28 @@ image = canvas.create_image(0, 0, anchor='nw', image=photo)
 canvas.place(x=425, y=325, anchor=CENTER)
 
 # Местоположение
-btn1 = Button(window, text="Солнце", fg="black", font=("Ariak Bold", 10), width=10,height=4, command=clicked)
-btn2 = Button(window, text="Информация", fg="Black", font=("Ariak Bold", 10), width=10,height=4, command=clicked_info)
-btn3 = Button(window, text="Выход", fg="Black", font=("Ariak Bold", 10), width=10,height=4, command=window.destroy)
+btn1 = Button(window, text="Солнце", fg="black", font=("Ariak Bold", 10), width=10, height=4, command=clicked_sun)
+btn2 = Button(window, text="Меркурий", fg="Black", font=("Ariak Bold", 10), width=10, height=4, command=clicked_mercury)
+btn3 = Button(window, text="Венера", fg="Black", font=("Ariak Bold", 10), width=10, height=4, command=clicked_venus)
+btn4 = Button(window, text="Земля", fg="Black", font=("Ariak Bold", 10), width=10, height=4, command=clicked_earth)
+btn5 = Button(window, text="Марс", fg="Black", font=("Ariak Bold", 10), width=10, height=4, command=clicked_mars)
+btn6 = Button(window, text="Юпитер", fg="Black", font=("Ariak Bold", 10), width=10, height=4, command=clicked_jupiter)
+btn7 = Button(window, text="Сатурн", fg="Black", font=("Ariak Bold", 10), width=10, height=4, command=clicked_saturn)
+btn8 = Button(window, text="Уран", fg="Black", font=("Ariak Bold", 10), width=10, height=4, command=clicked_uranus)
+btn9 = Button(window, text="Нептун", fg="Black", font=("Ariak Bold", 10), width=10, height=4, command=clicked_neptune)
+btn10 = Button(window, text="Плутон", fg="Black", font=("Ariak Bold", 10), width=10, height=4, command=clicked_pluto)
+btn11 = Button(window, text="Плутон", fg="Black", font=("Ariak Bold", 10), width=30, height=4, command=window.destroy)
 # Кнопка
-btn1.place(x = 125, y = 100, anchor=CENTER)
-btn2.place(x = 325, y = 100, anchor=CENTER)
-btn3.place(x = 525, y = 100, anchor=CENTER)
+btn1.place(x=125, y=100, anchor=CENTER)
+btn2.place(x=325, y=100, anchor=CENTER)
+btn3.place(x=525, y=100, anchor=CENTER)
+btn4.place(x=725, y=100, anchor=CENTER)
+btn5.place(x=125, y=250, anchor=CENTER)
+btn6.place(x=325, y=250, anchor=CENTER)
+btn7.place(x=525, y=250, anchor=CENTER)
+btn8.place(x=725, y=250, anchor=CENTER)
+btn9.place(x=325, y=400, anchor=CENTER)
+btn10.place(x=525, y=400, anchor=CENTER)
+btn11.place(x=425, y=550, anchor=CENTER)
 # testtest
 window.mainloop()
