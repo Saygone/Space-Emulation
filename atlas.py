d44e6e3fcceb6551a9c8f07640b8472720d3100f
Saygone <fordestiny1902@gmail.com>
2021-05-24 10:35:12 +0300

    Почти готово

--- atlas.py
+++ atlas.py
@@ -3,12 +3,16 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 
-#def clicked():
-    #import game
+def clicked():
+    import game
+
+
+def clicked_info():
+    exec(open('info.py').read())
 
 
 window = Tk()
-window.title("Атлас космических тел")
+window.title("Space emulation")
 window.geometry('850x650')
 
 canvas = tkinter.Canvas(window, height=650, width=850)
@@ -18,28 +22,12 @@ image = canvas.create_image(0, 0, anchor='nw', image=photo)
 canvas.place(x=425, y=325, anchor=CENTER)
 
 # Местоположение
-btn1 = Button(window, text="Солнце", fg="black", font=("Ariak Bold", 15), width=150,height=200, command=window.destroy)
-btn2 = Button(window, text="Меркурий", fg="Black", font=("Ariak Bold", 15), width=150,height=200, command=window.destroy)
-btn3 = Button(window, text="Венера", fg="Black", font=("Ariak Bold", 15), width=150,height=200, command=window.destroy)
-btn4 = Button(window, text="Земля", fg="black", font=("Ariak Bold", 15), width=150,height=200, command=window.destroy)
-btn5 = Button(window, text="Марс", fg="Black", font=("Ariak Bold", 15), width=150,height=200, command=window.destroy)
-btn6 = Button(window, text="Юпитер", fg="Black", font=("Ariak Bold", 15), width=150,height=200, command=window.destroy)
-btn7 = Button(window, text="Сатурн", fg="black", font=("Ariak Bold", 15), width=150,height=200, command=window.destroy)
-btn8 = Button(window, text="Уран", fg="Black", font=("Ariak Bold", 15), width=150,height=200, command=window.destroy)
-btn9 = Button(window, text="Нептун", fg="Black", font=("Ariak Bold", 15), width=150,height=200, command=window.destroy)
-btn10 = Button(window, text="Плутон", fg="Black", font=("Ariak Bold", 15), width=150,height=200, command=window.destroy)
-btn11 = Button(window, text="Выход", fg="Black", font=("Ariak Bold", 15), width=350,height=100, command=window.destroy)
+btn1 = Button(window, text="Солнце", fg="black", font=("Ariak Bold", 10), width=10,height=4, command=clicked)
+btn2 = Button(window, text="Информация", fg="Black", font=("Ariak Bold", 10), width=10,height=4, command=clicked_info)
+btn3 = Button(window, text="Выход", fg="Black", font=("Ariak Bold", 10), width=10,height=4, command=window.destroy)
 # Кнопка
 btn1.place(x = 125, y = 100, anchor=CENTER)
 btn2.place(x = 325, y = 100, anchor=CENTER)
 btn3.place(x = 525, y = 100, anchor=CENTER)
-btn4.place(x = 725, y = 100, anchor=CENTER)
-btn5.place(x = 125, y = 250, anchor=CENTER)
-btn6.place(x = 325, y = 250, anchor=CENTER)
-btn7.place(x = 525, y = 250, anchor=CENTER)
-btn8.place(x = 725, y = 250, anchor=CENTER)
-btn9.place(x = 325, y = 400, anchor=CENTER)
-btn10.place(x = 550, y = 400, anchor=CENTER)
-btn11.place(x = 425, y = 550, anchor=CENTER)
 # testtest
 window.mainloop()
